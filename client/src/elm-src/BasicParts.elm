affcd031f369fcb99226d8c223a0210ecf5efda9
narumincho <narumincho.starfy@gmail.com>
2019-06-24 11:42:57 +0900

    矢印を丸く

--- client/src/elm-src/BasicParts.elm
+++ client/src/elm-src/BasicParts.elm
@@ -61,7 +61,7 @@ backArrow =
         [ Svg.svg
             [ Svg.Attributes.viewBox "0 0 24 24" ]
             [ Svg.path
-                [ Svg.Attributes.d "M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
+                [ Svg.Attributes.d "M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"
                 , Svg.Attributes.fill "white"
                 ]
                 []
