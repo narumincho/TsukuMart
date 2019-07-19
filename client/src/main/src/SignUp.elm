856089b78356c74dbd2949455f70f75eb49e42e6
narumincho <narumincho.starfy@gmail.com>
2019-07-19 20:25:55 +0900

    新規登録フォームをElmに移行中

--- client/src/main/src/SignUp.elm
+++ client/src/main/src/SignUp.elm
@@ -5,6 +5,10 @@ import Browser
 import Data.ImageId
 import Html
 import Html.Attributes
+import Html.Events
+import Html.Keyed
+import Json.Decode
+import Page.Component.University as University
 import Page.Style
 
 
@@ -15,7 +19,10 @@ port load : String -> Cmd msg
 
 
 type Model
-    = Model Image
+    = Model
+        { image : Image
+        , university : University.Model
+        }
 
 
 type Image
@@ -25,6 +32,8 @@ type Image
 
 type Msg
     = InputImage String
+    | MsgByUniversity University.Msg
+    | Submit
 
 
 main : Program String Model Msg
@@ -39,49 +48,124 @@ main =
 
 init : String -> ( Model, Cmd Msg )
 init urlFragment =
-    ( Model (ServiceImage (Data.ImageId.fromString ""))
+    let
+        ( universityModel, universityEmission ) =
+            University.initModelNone
+    in
+    ( Model
+        { image = ServiceImage (Data.ImageId.fromString "")
+        , university = universityModel
+        }
     , load imageInputId
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
+update msg (Model rec) =
     case msg of
         InputImage imageDataUrl ->
-            ( Model (CustomizeImage imageDataUrl)
+            ( Model
+                { rec | image = CustomizeImage imageDataUrl }
+            , Cmd.none
+            )
+
+        MsgByUniversity universityMsg ->
+            ( Model
+                { rec | university = rec.university |> University.update universityMsg |> Tuple.first }
+            , Cmd.none
+            )
+
+        Submit ->
+            ( Model rec
             , Cmd.none
             )
 
 
 view : Model -> Html.Html Msg
-view model =
-    Html.div []
+view (Model { image, university }) =
+    Html.div
+        [ Html.Attributes.id "app" ]
         [ BasicParts.headerWithoutBackArrow
-        , Html.form
+        , Html.Keyed.node "form"
             [ Html.Attributes.style "padding" "64px 8px 8px 8px"
             , Html.Attributes.style "width" "100%"
             , Html.Attributes.style "max-width" "512px"
             , Html.Attributes.style "display" "grid"
             , Html.Attributes.style "gap" "16px"
             ]
-            []
-        , Html.h1
-            []
-            [ Html.text "新規登録!" ]
-        , Page.Style.formItem "学籍番号"
-            studentInputId
-            (inputTextHtml
-                [ Html.Attributes.id studentInputId
-                , Html.Attributes.type_ "text"
-                , Html.Attributes.required True
-                ]
+            ([ ( "title"
+               , Html.h1
+                    []
+                    [ Html.text "新規登録!" ]
+               )
+             , ( "studentIdView", studentIdView )
+             , ( "imageView", imageView image )
+             ]
+                ++ (University.view university
+                        |> List.map (Tuple.mapSecond (Html.map MsgByUniversity))
+                   )
+                ++ [ ( "submitButton", submitButtonView ) ]
             )
         ]
+
+
+studentIdView : Html.Html Msg
+studentIdView =
+    Page.Style.formItem "学籍番号"
+        studentInputId
+        (inputTextHtml
+            [ Html.Attributes.id studentInputId
+            , Html.Attributes.type_ "text"
+            , Html.Attributes.required True
+            ]
+        )
 
 
 studentInputId : String
 studentInputId =
     "studentId"
+
+
+imageView : Image -> Html.Html Msg
+imageView image =
+    Html.div
+        []
+        [ Html.label
+            [ Html.Attributes.for imageInputId ]
+            [ Html.img
+                [ Html.Attributes.src
+                    (case image of
+                        ServiceImage imageId ->
+                            Data.ImageId.toUrlString imageId
+
+                        CustomizeImage dataUrl ->
+                            dataUrl
+                    )
+                ]
+                []
+            ]
+        , Html.input
+            [ Html.Attributes.id imageInputId
+            , Html.Attributes.type_ "file"
+            , Html.Attributes.accept "image/*"
+            ]
+            []
+        ]
+
+
+submitButtonView : Html.Html Msg
+submitButtonView =
+    Html.button
+        [ Html.Events.custom
+            "click"
+            (Json.Decode.succeed
+                { message = Submit
+                , stopPropagation = True
+                , preventDefault = True
+                }
+            )
+        ]
+        [ Html.text "新規登録する" ]
 
 
 inputTextHtml : List (Html.Attribute Msg) -> Html.Html Msg
